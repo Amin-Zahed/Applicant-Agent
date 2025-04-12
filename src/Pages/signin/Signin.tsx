5de78a3eb3a3878573721598d39b6e579618306d
Amin Zahed <aminzahed99@gmail.com>
2025-04-12 16:23:02 +0330

    style: update border styles for Signin and Signup components for consistency

--- src/Pages/signin/Signin.tsx
+++ src/Pages/signin/Signin.tsx
@@ -2,11 +2,11 @@ import SigninForm from "@/components/signin-form";
 function Signin() {
   return (
     <div className="flex h-full w-full flex-col items-center justify-center">
-      <div className="flex flex-col gap-4 p-6 md:p-10 border border-accent-foreground rounded-md">
-        <div className="flex justify-center gap-2 md:justify-start"></div>
-        <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-xs">
-            <SigninForm />
+      <div className="flex flex-col gap-4 p-6 md:p-10 border-2 border-border rounded-2xl">
+        <div className="flex justify-center gap-2 md:justify-start "></div>
+        <div className="flex flex-1 items-center justify-center ">
+          <div className="w-full max-w-xs ">
+            <SigninForm className="" />
           </div>
         </div>
       </div>
