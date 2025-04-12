e0a195fabb67a3cd3d55f92afc4e573cbec7f88c
Amin Zahed <aminzahed99@gmail.com>
2025-04-12 17:16:18 +0330

    feat: enhance Signup and Signin components with layout adjustments and image integration

--- src/Pages/signup/Signup.tsx
+++ src/Pages/signup/Signup.tsx
@@ -1,14 +1,24 @@
 import SignupForm from "@/components/signup-form";
+import image from "@/assets/images/a modern dashboard design with Reacts Logo on the left side of the wall next to a monitor and TS logo on the right side of the wall next to the monitor.png";
+
 function Signup() {
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center">
-      <div className="flex flex-col gap-4 p-6 md:p-10 border-2 border-border rounded-2xl">
+    // <div className="flex h-full w-full flex-col items-center justify-center">
+    <div className="grid h-full lg:grid-cols-2 border-2 border-border rounded-2xl overflow-hidden m-4">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start"></div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
             <SignupForm />
           </div>
         </div>
+      </div>
+      <div className="relative hidden bg-muted lg:block">
+        <img
+          src={image}
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
       </div>
     </div>
   );
--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -39,82 +39,84 @@ function SignupForm({
 
   return (
     <form className={cn("flex flex-col gap-6", className)} {...props}>
-      <div className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-2xl font-bold">Create an account</h1>
-        <p className="text-balance text-sm text-muted-foreground">
-          Enter your user name below to sign up at this website
-        </p>
-      </div>
-      <div className="grid gap-6">
-        <div className="grid gap-2">
-          <Label htmlFor="username">User name</Label>
-          <Input
-            id="username"
-            type="username"
-            name="username"
-            maxLength={20}
-            minLength={3}
-            placeholder="AminZahed"
-            required
-            onInput={(e: ChangeEvent<HTMLInputElement>) =>
-              inputsHandler(e, /^(\w+(\w+|\s+))+[\w]+$/)
-            }
-          />
-          {formDatas.username === "notValid" && (
-            <Alert variant="destructive">
-              <AlertCircle className="h-4 w-4" />
-              <AlertTitle>Warning</AlertTitle>
-              <AlertDescription>
-                User name must be between 3 and 20 charactors contains uppercase
-                and/or lowercase letters and/or numbers and/or under score. also
-                you can to place white spaces between charactors
-              </AlertDescription>
-            </Alert>
-          )}
+      <fieldset id="main-signup-fieldset" className="flex flex-col gap-6">
+        <div className="flex flex-col items-center gap-2 text-center">
+          <h1 className="text-2xl font-bold">Create an account</h1>
+          <p className="text-balance text-sm text-muted-foreground">
+            Enter your user name below to sign up at this website
+          </p>
         </div>
-        <div className="grid gap-2">
-          <div className="flex items-center">
-            <Label htmlFor="password">Password</Label>
+        <div className="grid gap-6">
+          <div className="grid gap-2">
+            <Label htmlFor="username">User name</Label>
+            <Input
+              id="username"
+              type="username"
+              name="username"
+              maxLength={20}
+              minLength={3}
+              placeholder="AminZahed"
+              required
+              onInput={(e: ChangeEvent<HTMLInputElement>) =>
+                inputsHandler(e, /^(\w+(\w+|\s+))+[\w]+$/)
+              }
+            />
+            {formDatas.username === "notValid" && (
+              <Alert variant="destructive">
+                <AlertCircle className="h-4 w-4" />
+                <AlertTitle>Warning</AlertTitle>
+                <AlertDescription>
+                  User name must be between 3 and 20 charactors contains
+                  uppercase and/or lowercase letters and/or numbers and/or under
+                  score. also you can to place white spaces between charactors
+                </AlertDescription>
+              </Alert>
+            )}
           </div>
-          <Input
-            id="password"
-            type="password"
-            name="password"
-            maxLength={20}
-            minLength={6}
-            placeholder="******"
-            required
-            // className="w-[80%]"
-            onInput={(e: ChangeEvent<HTMLInputElement>) =>
-              inputsHandler(
-                e,
-                /(?=(\S*[0-9]))((?=\S*[a-zA-Z0-9])(?=\S*[A-Z])(?=\S*[a-z]))^\S{6,}$/
-              )
-            }
-          />
-          {/* <span className="w-[10%] bg-amber-400">
+          <div className="grid gap-2">
+            <div className="flex items-center">
+              <Label htmlFor="password">Password</Label>
+            </div>
+            <Input
+              id="password"
+              type="password"
+              name="password"
+              maxLength={20}
+              minLength={6}
+              placeholder="******"
+              required
+              // className="w-[80%]"
+              onInput={(e: ChangeEvent<HTMLInputElement>) =>
+                inputsHandler(
+                  e,
+                  /(?=(\S*[0-9]))((?=\S*[a-zA-Z0-9])(?=\S*[A-Z])(?=\S*[a-z]))^\S{6,}$/
+                )
+              }
+            />
+            {/* <span className="w-[10%] bg-amber-400">
             {showPassword ? (
               <Eye onClick={setShowPassword(false)} />
             ) : (
               <EyeOff onClick={setShowPassword(true)} />
             )}
           </span> */}
-          {formDatas.password === "notValid" && (
-            <Alert variant="destructive">
-              <AlertCircle className="h-4 w-4" />
-              <AlertTitle>Warning</AlertTitle>
-              <AlertDescription>
-                Password must be between 6 and 20 charactors That's for sure
-                contains minimum 1 number and 1 uppercase and 1 lowercase letter
-                and 1 special characters like @ or ~ or \ or ...
-              </AlertDescription>
-            </Alert>
-          )}
+            {formDatas.password === "notValid" && (
+              <Alert variant="destructive">
+                <AlertCircle className="h-4 w-4" />
+                <AlertTitle>Warning</AlertTitle>
+                <AlertDescription>
+                  Password must be between 6 and 20 charactors That's for sure
+                  contains minimum 1 number and 1 uppercase and 1 lowercase
+                  letter and 1 special characters like @ or ~ or \ or ...
+                </AlertDescription>
+              </Alert>
+            )}
+          </div>
+          <Button type="button" className="w-full cursor-pointer">
+            Sign up
+          </Button>
         </div>
-        <Button type="button" className="w-full cursor-pointer">
-          Sign up
-        </Button>
-      </div>
+      </fieldset>
     </form>
   );
 }
