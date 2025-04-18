2848109662ca9254f858d0b2c1c0283ee8838ed7
Amin Zahed <aminzahed99@gmail.com>
2025-04-18 15:12:07 +0330

    feat: integrate signup form validation to conditionally render navigation and signup components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-// import SigninedNavbar from "./components/signined-navbar";
+import SigninedNavbar from "./components/signined-navbar";
 import NotSigninedNavbar from "./components/not-signined-navbar";
 import routes from "./routes";
 import { useRoutes } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider";
+import useSignupForm from "./stores/useSignupForm";
 
 function App() {
   let router = useRoutes(routes);
+  const { signupFormIsValid } = useSignupForm();
 
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div id="App" className=" w-dvw h-dvh grid grid-cols-12 grid-rows-12">
         <header className="col-start-1 col-end-13 row-start-1 row-end-2">
-          {/* <SigninedNavbar /> */}
-          <NotSigninedNavbar />
+          {signupFormIsValid ? <SigninedNavbar /> : <NotSigninedNavbar />}
         </header>
         <main className="col-start-1 col-end-13 row-start-2 row-end-13 flex flex-col flex-nowrap items-center">
           {router}
--- src/Pages/signup/Signup.tsx
+++ src/Pages/signup/Signup.tsx
@@ -1,10 +1,17 @@
 import SignupForm from "@/components/signup-form";
 import image from "@/assets/images/a modern dashboard design with Reacts Logo on the left side of the wall next to a monitor and TS logo on the right side of the wall next to the monitor.png";
+import useSignupForm from "../../stores/useSignupForm";
 
 function Signup() {
+  const { signupFormIsValid } = useSignupForm();
+
   return (
     <div className="flex h-full w-full flex-col items-center justify-center">
-      <div className="flex h-full w-full overflow-auto scroll-auto">
+      <div
+        id="signup-form-container"
+        className="h-full w-full overflow-auto scroll-auto"
+        style={{ display: signupFormIsValid ? "none" : "flex" }}
+      >
         <div className="flex flex-col gap-4 p-6 md:p-10 lg:w-1/2 w-full h-full">
           <div className="flex justify-center gap-2 md:justify-start"></div>
           <div className="flex flex-1 items-center justify-center">
@@ -21,6 +28,11 @@ function Signup() {
           />
         </div>
       </div>
+      <div
+        id="signuped-card-container"
+        className="h-full w-full overflow-auto scroll-auto bg-lime-400"
+        style={{ display: signupFormIsValid ? "flex" : "none" }}
+      ></div>
     </div>
   );
 }
