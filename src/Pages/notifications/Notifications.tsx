16b8c15819e6707707b3bd16ba60e03eac1639a4
Amin Zahed <aminzahed99@gmail.com>
2025-05-25 19:14:25 +0330

    feat: implement Notifications component with Accordion for displaying notifications

--- src/Pages/notifications/Notifications.tsx
+++ src/Pages/notifications/Notifications.tsx
@@ -1,8 +1,38 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 const Notifications = () => {
   return (
-    <>
-      <h1>Notifications page</h1>
-    </>
+    <div
+      id="notifications-Container"
+      className="p-2 w-[320px] sm:w-[600px] md:w-[700px] lg:w-[950px] xl:w-[1150px] 2xl:w-[1200px] h-full overflow-y-auto scroll-auto"
+    >
+      <Accordion type="single" collapsible className="w-full">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>Is it styled?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It comes with default styles that matches the other
+            components&apos; aesthetic.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>Is it animated?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It's animated by default, but you can disable it if you prefer.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
   );
 };
 
