9510ccf74e33d0dd57ff16d399163ceeb0bc1c7d
Amin Zahed <aminzahed99@gmail.com>
2025-05-25 18:52:31 +0330

    refactor: remove unnecessary fragment and clean up NotFound component structure

--- src/Pages/notFound/NotFound.tsx
+++ src/Pages/notFound/NotFound.tsx
@@ -3,32 +3,22 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full">
-        <body class="h-full">
-        ```
-      */}
-      <main className="grid min-h-full place-items-center bg-background px-6 py-24 sm:py-32 lg:px-8">
-        <div className="text-center">
-          <p className="text-base font-semibold text-muted-foreground">404</p>
-          <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-secondary-foreground sm:text-7xl">
-            Page not found
-          </h1>
-          <p className="mt-6 text-lg font-medium text-pretty text-muted-foreground sm:text-xl/8">
-            Sorry, we couldn’t find the page you’re looking for.
-          </p>
-          <div className="mt-10 flex items-center justify-center gap-x-6">
-            <Link to="/">
-              <Button>Go back home</Button>
-            </Link>
-          </div>
+    <main className="grid min-h-full place-items-center bg-background px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <p className="text-base font-semibold text-muted-foreground">404</p>
+        <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-secondary-foreground sm:text-7xl">
+          Page not found
+        </h1>
+        <p className="mt-6 text-lg font-medium text-pretty text-muted-foreground sm:text-xl/8">
+          Sorry, we couldn’t find the page you’re looking for.
+        </p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Link to="/">
+            <Button>Go back home</Button>
+          </Link>
         </div>
-      </main>
-    </>
+      </div>
+    </main>
   );
 };
 
