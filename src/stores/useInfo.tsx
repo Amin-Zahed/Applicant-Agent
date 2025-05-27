82050f3c8927e5aee44750663e538e8d4570ee12
Amin Zahed <aminzahed99@gmail.com>
2025-05-27 14:09:55 +0330

    feat: add initial implementation of useInfo store with Zustand

---
+++ src/stores/useInfo.tsx
@@ -0,0 +1,30 @@
+// import { create } from "zustand";
+
+// // export type Data = {
+// //   id: string;
+// //   status: "pending" | "rejected" | "interview";
+// //   employer: string;
+// //   baseResume: string;
+// //   changedResume: string;
+//   // time: number;
+// // };
+
+// interface UseInfo {
+// //   data: Data[];
+// //   updateData: (datas: Data[]) => void;
+// //   deleteData: (index: number) => void;
+// }
+
+// const useInfo = create<UseInfo>((set, get) => ({
+// //   data: [],
+
+// //   updateData: (datas) => set({ data: datas }),
+// //   deleteData: (index) => {
+// //     get().data.splice(index, 1);
+// //     const allDatas = get().data;
+// //     set({ data: allDatas });
+//     // set({ cloneDrafts: allDrafts });
+//   },
+// }));
+
+// export default useInfo;
