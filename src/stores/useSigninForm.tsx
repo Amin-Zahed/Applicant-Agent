35d4cf104d738ce5aed26c0ac851e9ad1a451ec9
Amin Zahed <aminzahed99@gmail.com>
2025-06-02 16:07:34 +0330

    refactor: clean up useSigninForm store by removing commented code and enhancing input validation

--- src/stores/useSigninForm.tsx
+++ src/stores/useSigninForm.tsx
@@ -1,17 +1,38 @@
 import { create } from "zustand";
 
 interface UseSigninForm {
-  //   isLogin: boolean;
-  //   setIsLogin: (isLogin: boolean) => void;
+  usernameInputValue: null | undefined | string;
+  passwordInputValue: null | undefined | string;
+  setUsernameInputValue: (value: null | undefined | string) => void;
+  setPasswordInputValue: (value: null | undefined | string) => void;
 }
 
+const usernameRegex: RegExp = /^[A-Za-z0-9._\-\+]{3,}$/;
+const passwordRegex: RegExp =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()[\]{}\-_=+|;:'",.<>/?\\`~])[\s\S]{4,}$/;
+
 const useSigninForm = create<UseSigninForm>((set) => ({
-  //   isLogin: false,
-  //   setIsLogin: (isLogin) => {
-  //     {
-  //       set(() => ({ isLogin: isLogin }));
-  //     }
-  //   },
+  usernameInputValue: null,
+  passwordInputValue: null,
+
+  setUsernameInputValue: (value) => {
+    if (value && usernameRegex.test(value)) {
+      set({ usernameInputValue: value });
+    } else if (value && !usernameRegex.test(value)) {
+      set({ usernameInputValue: undefined });
+    } else {
+      set({ usernameInputValue: null });
+    }
+  },
+  setPasswordInputValue: (value) => {
+    if (value && passwordRegex.test(value)) {
+      set({ passwordInputValue: value });
+    } else if (value && !passwordRegex.test(value)) {
+      set({ passwordInputValue: undefined });
+    } else {
+      set({ passwordInputValue: null });
+    }
+  },
 }));
 
 export default useSigninForm;
