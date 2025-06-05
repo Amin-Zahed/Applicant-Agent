b9111e723588d4e29689013b80fb47c865343f2b
Amin Zahed <aminzahed99@gmail.com>
2025-06-05 17:13:25 +0330

    refactor: streamline notifications handling in Notifications component

--- src/Pages/dashboard/dashboard.tsx
+++ src/Pages/dashboard/dashboard.tsx
@@ -14,11 +14,34 @@ import { Label } from "@/components/ui/label";
 import { Link, Outlet, useLocation } from "react-router-dom";
 import useAppSidebar from "@/stores/useAppSidebar";
 import { useIsMobile } from "@/hooks/use-mobile";
+import useNotifications from "@/stores/useNotifications";
+import { useEffect } from "react";
 
 function Dashboard() {
   const location = useLocation();
   const { sidebarIsOpen } = useAppSidebar();
   const isMobile = useIsMobile();
+  const { notifications, setNotifications, setNumberOfNots } =
+    useNotifications();
+
+  useEffect(() => {
+    setNotifications([
+      { id: 1, title: "Notification 1", content: "Content for notification 1" },
+      { id: 2, title: "Notification 2", content: "Content for notification 2" },
+      { id: 3, title: "Notification 3", content: "Content for notification 3" },
+      { id: 4, title: "Notification 4", content: "Content for notification 4" },
+      { id: 5, title: "Notification 5", content: "Content for notification 5" },
+      { id: 6, title: "Notification 6", content: "Content for notification 6" },
+      { id: 7, title: "Notification 7", content: "Content for notification 7" },
+      { id: 8, title: "Notification 8", content: "Content for notification 8" },
+      { id: 9, title: "Notification 9", content: "Content for notification 9" },
+    ]);
+    setNumberOfNots();
+  }, []);
+
+  useEffect(() => {
+    setNumberOfNots();
+  }, [notifications]);
 
   return (
     <div id="dashboard" className="w-full h-full flex flex-col overflow-auto">
