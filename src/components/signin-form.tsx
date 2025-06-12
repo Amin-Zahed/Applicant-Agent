a60b426aed62e29949d9da1ec6a3581754411e94
Amin Zahed <aminzahed99@gmail.com>
2025-06-12 12:36:03 +0330

    fix: remove unused SVG imports and adjust icon dimensions for GitHub and Google sign-in buttons

--- src/components/signin-form.tsx
+++ src/components/signin-form.tsx
@@ -3,7 +3,6 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Link } from "react-router-dom";
-// import { Chrome, Github } from "lucide-react";
 import useSigninForm from "@/stores/useSigninForm";
 import useLogin from "@/stores/useLogin";
 
@@ -92,23 +91,11 @@ function SigninForm({
               fill="currentColor"
             />
           </svg>
-          {/* <Github /> */}
           Sign in with GitHub
         </Button>
         <Button variant="outline" className="w-full flex items-center gap-2">
-          {/* <svg
-            viewBox="-2.4 -2.4 28.80 28.80"
-            fill="var(--foreground)"
-            xmlns="http://www.w3.org/2000/svg"
-            fontSize={40}
-          >
-            <path
-              d="M19.76 10.77L19.67 10.42H12.23V13.58H16.68C16.4317 14.5443 15.8672 15.3974 15.0767 16.0029C14.2863 16.6084 13.3156 16.9313 12.32 16.92C11.0208 16.9093 9.77254 16.4135 8.81999 15.53C8.35174 15.0685 7.97912 14.5191 7.72344 13.9134C7.46777 13.3077 7.33407 12.6575 7.33 12C7.34511 10.6795 7.86792 9.41544 8.79 8.47002C9.7291 7.58038 10.9764 7.08932 12.27 7.10002C13.3779 7.10855 14.4446 7.52101 15.27 8.26002L17.47 6.00002C16.02 4.70638 14.1432 3.9941 12.2 4.00002C11.131 3.99367 10.0713 4.19793 9.08127 4.60115C8.09125 5.00436 7.19034 5.59863 6.43 6.35002C4.98369 7.8523 4.16827 9.85182 4.15152 11.9371C4.13478 14.0224 4.918 16.0347 6.34 17.56C7.12784 18.3449 8.06422 18.965 9.09441 19.3839C10.1246 19.8029 11.2279 20.0123 12.34 20C13.3484 20.0075 14.3479 19.8102 15.2779 19.42C16.2078 19.0298 17.0488 18.4549 17.75 17.73C19.1259 16.2171 19.8702 14.2347 19.83 12.19C19.8408 11.7156 19.8174 11.2411 19.76 10.77Z"
-              fill="var(--background)"
-            />
-          </svg> */}
           <svg
-            viewBox="-26.2 -26.2 314.40 314.40"
+            viewBox="-52.4 -52.4 366.80 366.80"
             xmlns="http://www.w3.org/2000/svg"
             preserveAspectRatio="xMidYMid"
             fill="var(--background)"
@@ -122,11 +109,11 @@ function SigninForm({
               transform="translate(0,0), scale(1)"
             >
               <rect
-                x="-26.2"
-                y="-26.2"
-                width="314.40"
-                height="314.40"
-                rx="157.2"
+                x="-52.4"
+                y="-52.4"
+                width="366.80"
+                height="366.80"
+                rx="183.4"
                 fill="var(--foreground)"
                 // strokewidth="0"
               ></rect>
@@ -157,7 +144,6 @@ function SigninForm({
               ></path>
             </g>
           </svg>
-          {/* <Chrome /> */}
           Sign in with Google
         </Button>
       </div>
