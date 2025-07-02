7392ca185537f3bf18f87d5914f3859bd4e74d3d
Amin Zahed <aminzahed99@gmail.com>
2025-07-02 19:23:52 +0330

    refactor(Navbar): update logo rendering logic to improve conditional rendering and maintain consistency

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -15,11 +15,17 @@ const Navbar = () => {
     <Menubar className="flex flex-nowrap justify-between w-full h-full items-center pr-3 sticky top-0 z-50">
       <div className="flex md:w-1/3 lg:w-1/12 2xl:1/12 w-1/3 justify-between items-center">
         <MenubarMenu>
-          <Link to={isLogin ? "/dashboard" : "/"}>
-            <MenubarTrigger>
+          {isLogin ? (
+            <MenubarTrigger className="cursor-default">
               <LogoSvg />
             </MenubarTrigger>
-          </Link>
+          ) : (
+            <Link to="/">
+              <MenubarTrigger>
+                <LogoSvg />
+              </MenubarTrigger>
+            </Link>
+          )}
         </MenubarMenu>
       </div>
 
